773964f50ed59a2837bab78f5f2a29472762e04d
Dessalines <dessalines@users.noreply.github.com>
2022-03-13 21:49:57 +0000

    Fix image height in landscape mode. Fixes #122 (#125)

--- app/src/main/java/com/jerboa/ui/components/common/PictrsImage.kt
+++ app/src/main/java/com/jerboa/ui/components/common/PictrsImage.kt
@@ -18,9 +18,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import coil.size.OriginalSize
@@ -94,6 +96,9 @@ fun PictrsUrlImage(
     url: String,
     modifier: Modifier = Modifier
 ) {
+    val configuration = LocalConfiguration.current
+    val screenHeight = (configuration.screenHeightDp - 150).dp
+
     Image(
         painter = rememberImagePainter(
             data = pictrsImageThumbnail(url, MAX_IMAGE_SIZE),
@@ -104,9 +109,11 @@ fun PictrsUrlImage(
                 transformations(RoundedCornersTransformation(12f))
             },
         ),
-        contentScale = ContentScale.FillWidth,
+        contentScale = ContentScale.Fit,
         contentDescription = null,
-        modifier = modifier,
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(0.dp, screenHeight)
     )
 }
 
