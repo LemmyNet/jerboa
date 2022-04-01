4f039ccaa1d2d990eeacae9306f08f570919b5bc
Dessalines <tyhou13@gmx.com>
2022-04-01 15:12:19 -0400

    Fixing install script.

--- deploy_to_device.sh
+++ deploy_to_device.sh
@@ -5,11 +5,8 @@ port=$1
 
 # Get port
 
-# Build
-./gradlew assembleDebug
+# Build and push
+./gradlew installDebug
 
-# Push the apk
-adb -t $port install -r app/build/outputs/apk/debug/app-debug.apk
-
-# Run jhe app
-adb -t $port shell monkey -p com.jerboa 1
+# Run the app
+adb shell monkey -p com.jerboa 1
