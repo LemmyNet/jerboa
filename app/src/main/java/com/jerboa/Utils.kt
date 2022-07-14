LemmyNet/jerboa

commit 5dbbdde87481799b4ddfb3105ffc418ba5f1389f

fix signout (#180)
* fix signout

* fix lint

* fix lint again :(

app/src/main/java/com/jerboa/Utils.kt
@@ -23,6 +23,7 @@ import androidx.core.graphics.ColorUtils
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.jerboa.api.API
+import com.jerboa.api.DEFAULT_INSTANCE
 import com.jerboa.datatypes.* // ktlint-disable no-unused-imports
 import com.jerboa.datatypes.api.GetUnreadCountResponse
 import com.jerboa.db.Account
@@ -529,6 +530,7 @@ fun fetchInitialData(
         homeViewModel.fetchUnreadCounts(account = account)
     } else {
         Log.d("jerboa", "Fetching posts for anonymous user")
+        API.changeLemmyInstance(DEFAULT_INSTANCE)
         homeViewModel.fetchPosts(
             account = account,
             clear = true,

app/src/main/java/com/jerboa/ui/components/home/HomeViewModel.kt
@@ -43,8 +43,8 @@ class HomeViewModel : ViewModel() {
         account: Account?,
         nextPage: Boolean = false,
         clear: Boolean = false,
-        changeListingType: ListingType? = null,
-        changeSortType: SortType? = null,
+        changeListingType: ListingType? = ListingType.Local,
+        changeSortType: SortType? = SortType.Active,
         ctx: Context? = null,
     ) {
         fetchPostsRoutine(

app/src/main/java/com/jerboa/ui/components/home/HomeActivity.kt
@@ -340,6 +340,13 @@ fun MainDrawer(
                     if (updatedList.isNotEmpty()) {
                         accountViewModel.setCurrent(updatedList[0].id)
                     }
+                    fetchInitialData(
+                        account = updatedList.getOrNull(0),
+                        siteViewModel = siteViewModel,
+                        homeViewModel = homeViewModel,
+                    )
+
+                    closeDrawer(scope, scaffoldState)
                 }
             }
         },

commit 75ee037dda6553d7617133d222557bbd4563cfa3

fix a bug i introduced in #180

app/src/main/java/com/jerboa/Utils.kt
@@ -544,6 +544,8 @@ fun fetchInitialData(
         homeViewModel.fetchPosts(
             account = account,
             clear = true,
+            changeListingType = ListingType.Local,
+            changeSortType = SortType.Active,
         )
     }
 

app/src/main/java/com/jerboa/ui/components/home/HomeViewModel.kt
@@ -43,8 +43,8 @@ class HomeViewModel : ViewModel() {
         account: Account?,
         nextPage: Boolean = false,
         clear: Boolean = false,
-        changeListingType: ListingType? = ListingType.Local,
-        changeSortType: SortType? = SortType.Active,
+        changeListingType: ListingType? = null,
+        changeSortType: SortType? = null,
         ctx: Context? = null,
     ) {
         fetchPostsRoutine(