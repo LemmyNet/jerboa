049a04b921019fdf9e506f0c1abe920be36e6d56
Dessalines <tyhou13@gmx.com>
2022-12-18 15:29:30 -0500

    Organizing imports.

--- app/src/main/java/com/jerboa/ui/components/privatemessage/PrivateMessage.kt
+++ app/src/main/java/com/jerboa/ui/components/privatemessage/PrivateMessage.kt
@@ -1,6 +1,10 @@
 package com.jerboa.ui.components.privatemessage
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
