d5ca21cadda9a7a798815196964e6a89a7af31c4
maarten.vercruysse <maarten.vercruysse@outlook.be>
2023-06-07 21:12:55 +0200

    Fix for autofill issues (#439)

--- app/src/main/java/com/jerboa/ui/components/login/Login.kt
+++ app/src/main/java/com/jerboa/ui/components/login/Login.kt
@@ -17,7 +17,16 @@ import androidx.compose.material3.ExposedDropdownMenuDefaults.TrailingIcon
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.autofill.AutofillNode
+import androidx.compose.ui.autofill.AutofillType
+import androidx.compose.ui.composed
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalAutofill
+import androidx.compose.ui.platform.LocalAutofillTree
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -32,6 +41,29 @@ import com.jerboa.db.Account
 
 val BANNED_INSTANCES = listOf("wolfballs.com")
 
+@OptIn(ExperimentalComposeUiApi::class)
+fun Modifier.autofill(
+    autofillTypes: List<AutofillType>,
+    onFill: ((String) -> Unit),
+) = composed {
+    val autofill = LocalAutofill.current
+    val autofillNode = AutofillNode(onFill = onFill, autofillTypes = autofillTypes)
+    LocalAutofillTree.current += autofillNode
+
+    this.onGloballyPositioned {
+        autofillNode.boundingBox = it.boundsInWindow()
+    }.onFocusChanged { focusState ->
+        autofill?.run {
+            if (focusState.isFocused) {
+                requestAutofillForNode(autofillNode)
+            } else {
+                cancelAutofillForNode(autofillNode)
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun MyTextField(
     label: String,
@@ -50,9 +82,14 @@ fun MyTextField(
             keyboardType = KeyboardType.Text,
             autoCorrect = false,
         ),
+        modifier = Modifier.autofill(
+            autofillTypes = listOf(AutofillType.EmailAddress, AutofillType.Username),
+            onFill = onValueChange,
+        ),
     )
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun PasswordField(
     password: String,
@@ -80,6 +117,10 @@ fun PasswordField(
                 Icon(imageVector = image, "")
             }
         },
+        modifier = Modifier.autofill(
+            autofillTypes = listOf(AutofillType.Password),
+            onFill = onValueChange,
+        ),
     )
 }
 
