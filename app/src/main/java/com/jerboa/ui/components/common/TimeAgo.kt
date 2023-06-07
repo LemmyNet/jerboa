cb28a3fb9026f129d1459c2463f4a4e4148df03e
Kevin Phoenix <kevin@kphoenix.us>
2023-06-07 13:12:48 -0700

    Fix lint

--- app/src/main/java/com/jerboa/ui/components/common/TimeAgo.kt
+++ app/src/main/java/com/jerboa/ui/components/common/TimeAgo.kt
@@ -18,7 +18,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
