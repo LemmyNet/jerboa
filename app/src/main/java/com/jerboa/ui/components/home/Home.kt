852ce5cad67759b46688433a5be0a1cb06c0880b
Felipe Martinez <pipernene@gmail.com>
2023-06-09 22:27:47 +0200

    Remove unused import

--- app/src/main/java/com/jerboa/ui/components/home/Home.kt
+++ app/src/main/java/com/jerboa/ui/components/home/Home.kt
@@ -57,7 +57,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
