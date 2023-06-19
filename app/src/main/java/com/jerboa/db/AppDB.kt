009bfc8b9b1543894c5c2838935aefb24368d644
yate <mwyeatts@gmail.com>
2023-06-19 11:14:21 -0400

    add MIGRATION_15_16

--- app/src/main/java/com/jerboa/db/AppDB.kt
+++ app/src/main/java/com/jerboa/db/AppDB.kt
@@ -476,6 +476,7 @@ abstract class AppDB : RoomDatabase() {
                         MIGRATION_12_13,
                         MIGRATION_13_14,
                         MIGRATION_14_15,
+                        MIGRATION_15_16,
                     )
                     // Necessary because it can't insert data on creation
                     .addCallback(object : Callback() {
