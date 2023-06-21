ea89eef93b9924a3cac1b8a6be441ae12f144178
CharlieGitDB <charliedeveloperaccess@gmail.com>
2023-06-21 10:17:46 -0500

    fix linter issue

--- app/src/main/java/com/jerboa/ui/components/common/SwipeToNavigateBack.kt
+++ app/src/main/java/com/jerboa/ui/components/common/SwipeToNavigateBack.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
 
-
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SwipeToNavigateBack(
