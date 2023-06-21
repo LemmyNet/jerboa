453a128ce99b70627bdf3d469aa3236a378948ae
CharlieGitDB <charliedeveloperaccess@gmail.com>
2023-06-20 20:57:41 -0500

    remove unused imports

--- app/src/main/java/com/jerboa/ui/components/common/SwipeToNavigateBack.kt
+++ app/src/main/java/com/jerboa/ui/components/common/SwipeToNavigateBack.kt
@@ -1,30 +1,16 @@
 package com.jerboa.ui.components.common
 
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
-import androidx.compose.foundation.gestures.rememberScrollableState
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.SwipeableDefaults
-import androidx.compose.material.Text
-import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.consumeAllChanges
-import androidx.compose.ui.input.pointer.consumePositionChange
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.input.pointer.positionChange
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -36,35 +22,31 @@ fun SwipeToNavigateBack(
     navController: NavController,
     content: @Composable () -> Unit
 ) {
-    val swipeThreshold = 150
-    val startX = remember { mutableStateOf(0f) }
-
     if (useSwipeBack) {
+        val offsetX = remember { mutableStateOf(0f) }
+        val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+
         Box(
             modifier = Modifier
                 .fillMaxSize()
+                .offset { IntOffset(offsetX.value.roundToInt(), 0) }
                 .pointerInput(Unit) {
-                    detectHorizontalDragGestures { change, dragAmount ->
-                        if (change.position.x == 0f) {
-                            startX.value = change.position.x
-                        }
-
-                        val deltaX = change.position.x - startX.value
-
-                        when {
-                            deltaX >= swipeThreshold -> {
-                                // Swipe from left to right detected
-                                navController.navigateUp()
-                                change.consume()
-                            }
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+                        if (dragAmount.x > 0) {
+                            offsetX.value = dragAmount.x
                         }
                     }
                 }
         ) {
             content()
+
+            val swipeThreshold: Dp = (screenWidth.value * 0.8).dp
+            if (offsetX.value > swipeThreshold.value) {
+                navController.navigateUp()
+            }
         }
     } else {
         content()
     }
-
 }
