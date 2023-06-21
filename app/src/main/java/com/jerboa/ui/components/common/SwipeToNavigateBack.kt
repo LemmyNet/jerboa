e6732ec7df7d6463f4f840587c1d2ebab3974b2d
CharlieGitDB <charliedeveloperaccess@gmail.com>
2023-06-21 09:54:33 -0500

    change to use SwipeToDismiss composable

--- app/src/main/java/com/jerboa/ui/components/common/SwipeToNavigateBack.kt
+++ app/src/main/java/com/jerboa/ui/components/common/SwipeToNavigateBack.kt
@@ -4,21 +4,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.rememberSwipeableState
-import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalDensity
 import androidx.navigation.NavController
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.layout.offset
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.DismissValue
 import androidx.compose.material.FractionalThreshold
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.ui.unit.dp
+import androidx.compose.material.SwipeToDismiss
+import androidx.compose.material.rememberDismissState
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.IntOffset
-import kotlin.math.roundToInt
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -26,31 +20,30 @@ fun SwipeToNavigateBack(
     navController: NavController,
     content: @Composable () -> Unit,
 ) {
-    val swipeableState = rememberSwipeableState(0)
-    val screenWidthPx = with(LocalDensity.current) { LocalConfiguration.current.screenWidthDp.dp.toPx() }
-
-    if (swipeableState.isAnimationRunning) {
-        DisposableEffect(Unit) {
-            onDispose {
-                if (swipeableState.currentValue == 1) {
-                    navController.navigateUp();
+    val dismissState = rememberDismissState(
+        confirmStateChange = {
+            when (it) {
+                DismissValue.DismissedToEnd -> {
+                    navController.navigateUp()
+                    true
                 }
+                else -> { false }
             }
         }
-    }
-
-    Box(
-        modifier = Modifier
-            .background(Color.Transparent)
-            .fillMaxSize()
-            .offset { IntOffset(swipeableState.offset.value.roundToInt(), 0) }
-            .swipeable(
-                state = swipeableState,
-                anchors = mapOf(0f to 0, screenWidthPx to 1),
-                thresholds = { _, _ -> FractionalThreshold(0.9f) },
-                orientation = Orientation.Horizontal,
+    )
+    SwipeToDismiss(
+        state = dismissState,
+        background = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Transparent)
             )
-    ) {
-        content()
-    }
+        },
+        dismissContent = {
+            content()
+        },
+        directions = setOf(DismissDirection.StartToEnd),
+        dismissThresholds = { FractionalThreshold(0.8f) }
+    )
 }
