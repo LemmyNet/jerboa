a299c4a9105a896831a68f39d21203ac2ff57952
CharlieGitDB <charliedeveloperaccess@gmail.com>
2023-06-21 10:04:14 -0500

    fix linter, fix imports

--- app/src/main/java/com/jerboa/ui/components/common/SwipeToNavigateBack.kt
+++ app/src/main/java/com/jerboa/ui/components/common/SwipeToNavigateBack.kt
@@ -3,16 +3,17 @@ package com.jerboa.ui.components.common
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
 import androidx.compose.material.DismissDirection
 import androidx.compose.material.DismissValue
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FractionalThreshold
 import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.rememberDismissState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavController
+
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -29,21 +30,22 @@ fun SwipeToNavigateBack(
                 }
                 else -> { false }
             }
-        }
+        },
     )
+
     SwipeToDismiss(
         state = dismissState,
         background = {
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .background(Color.Transparent)
+                    .background(Color.Transparent),
             )
         },
         dismissContent = {
             content()
         },
         directions = setOf(DismissDirection.StartToEnd),
-        dismissThresholds = { FractionalThreshold(0.8f) }
+        dismissThresholds = { FractionalThreshold(0.8f) },
     )
 }
