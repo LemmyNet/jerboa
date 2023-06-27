2f5f5cea376309441fecfe5944e92d906daa3887
Kenny Sanders <kensand@users.noreply.github.com>
2023-06-26 20:34:32 -0400

    Fix typos from merge

--- app/src/main/java/com/jerboa/MainActivity.kt
+++ app/src/main/java/com/jerboa/MainActivity.kt
@@ -18,9 +18,9 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
-import androidx.co`mpose.runtime.remember
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.viewmodel.compose.vie`wModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.navArgument
 import androidx.navigation.navDeepLink
 import androidx.navigation.navigation
