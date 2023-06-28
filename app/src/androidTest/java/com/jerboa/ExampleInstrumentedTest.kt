9f8c4f07a3482564fdf1ce92ab362c9eee01c6d2
maarten.vercruysse <maarten.vercruysse@outlook.be>
2023-06-28 10:16:47 +0200

    Add woodpecker test

--- app/src/androidTest/java/com/jerboa/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/jerboa/ExampleInstrumentedTest.kt
@@ -2,7 +2,7 @@ package com.jerboa
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.jerboa", appContext.packageName)
+        assertTrue(appContext.packageName.startsWith("com.jerboa"))
     }
 }
