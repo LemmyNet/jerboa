bec83b64e635cd523fd7c0230a06d7069e43530b
maarten.vercruysse <maarten.vercruysse@outlook.be>
2023-06-28 09:54:28 +0200

    Add schemas

--- app/src/main/java/com/jerboa/db/AppDB.kt
+++ app/src/main/java/com/jerboa/db/AppDB.kt
@@ -274,10 +274,10 @@ class AppSettingsRepository(
 
 val MIGRATION_1_2 = object : Migration(1, 2) {
     override fun migrate(database: SupportSQLiteDatabase) {
-//        database.execSQL(
-//            "alter table account add column default_listing_type INTEGER NOT " +
-//                "NULL default 0",
-//        )
+        database.execSQL(
+            "alter table account add column default_listing_type INTEGER NOT " +
+                "NULL default 0",
+        )
         database.execSQL(
             "alter table account add column default_sort_type INTEGER NOT " +
                 "NULL default 0",
