1eac8968505adde1eb25f673585de92adf844373
maarten.vercruysse <maarten.vercruysse@outlook.be>
2023-07-03 17:48:00 +0200

    Fix deprecations

--- app/src/main/java/com/jerboa/ui/components/home/BottomNavActivity.kt
+++ app/src/main/java/com/jerboa/ui/components/home/BottomNavActivity.kt
@@ -24,10 +24,10 @@ import androidx.compose.ui.semantics.testTagsAsResourceId
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import arrow.core.Either
-import com.google.accompanist.navigation.animation.AnimatedNavHost
-import com.google.accompanist.navigation.animation.composable
-import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import com.jerboa.api.ApiState
 import com.jerboa.db.AccountViewModel
 import com.jerboa.db.AppSettings
@@ -64,7 +64,7 @@ fun BottomNavActivity(
     val ctx = LocalContext.current
     val scope = rememberCoroutineScope()
 
-    val bottomNavController = rememberAnimatedNavController()
+    val bottomNavController = rememberNavController()
     var selectedTab by rememberSaveable { mutableStateOf(BottomNavTab.Home) }
     val onSelectTab = { tab: BottomNavTab ->
         if (tab.needsLogin() && account == null) {
@@ -127,7 +127,7 @@ fun BottomNavActivity(
                         padding.calculateBottomPadding()
                     }
 
-                AnimatedNavHost(
+                NavHost(
                     navController = bottomNavController,
                     startDestination = BottomNavTab.Home.name,
                     modifier = Modifier.padding(bottom = bottomPadding),
