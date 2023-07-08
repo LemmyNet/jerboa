d0c7f3c36aa601cbb4789ff315d2ce68c702ca65
yate <mwyeatts@gmail.com>
2023-07-07 21:38:43 -0400

    merge main/fix conflicts

--- app/src/main/java/com/jerboa/db/AppDB.kt
+++ app/src/main/java/com/jerboa/db/AppDB.kt
@@ -58,22 +58,7 @@ abstract class AppDB : RoomDatabase() {
                     "jerboa",
                 )
                     .addMigrations(
-                        MIGRATION_1_2,
-                        MIGRATION_2_3,
-                        MIGRATION_3_4,
-                        MIGRATION_4_5,
-                        MIGRATION_5_6,
-                        MIGRATION_6_7,
-                        MIGRATION_7_8,
-                        MIGRATION_8_9,
-                        MIGRATION_9_10,
-                        MIGRATION_10_11,
-                        MIGRATION_11_12,
-                        MIGRATION_12_13,
-                        MIGRATION_13_14,
-                        MIGRATION_14_15,
-                        MIGRATION_15_16,
-                        MIGRATION_16_17,
+                        *MIGRATIONS_LIST,
                     )
                     // Necessary because it can't insert data on creation
                     .addCallback(object : Callback() {
--- app/src/main/java/com/jerboa/db/AppDBMigrations.kt
+++ app/src/main/java/com/jerboa/db/AppDBMigrations.kt
@@ -201,3 +201,23 @@ val MIGRATION_16_17 = object : Migration(16, 17) {
         )
     }
 }
+
+// Don't forget to test your migration with `./gradlew app:connectAndroidTest`
+val MIGRATIONS_LIST = arrayOf(
+    MIGRATION_1_2,
+    MIGRATION_2_3,
+    MIGRATION_3_4,
+    MIGRATION_4_5,
+    MIGRATION_5_6,
+    MIGRATION_6_7,
+    MIGRATION_7_8,
+    MIGRATION_8_9,
+    MIGRATION_9_10,
+    MIGRATION_10_11,
+    MIGRATION_11_12,
+    MIGRATION_12_13,
+    MIGRATION_13_14,
+    MIGRATION_14_15,
+    MIGRATION_15_16,
+    MIGRATION_16_17,
+)
