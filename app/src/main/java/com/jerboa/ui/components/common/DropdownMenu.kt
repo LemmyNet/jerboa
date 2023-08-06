0f7c37d00b805b3512e329ab402bdf12edc32e18
Cameron Porter <camporter1@gmail.com>
2023-08-06 17:39:48 -0500

    Add text style to dropdown menu items.

--- app/src/main/java/com/jerboa/ui/components/common/DropdownMenu.kt
+++ app/src/main/java/com/jerboa/ui/components/common/DropdownMenu.kt
@@ -1,6 +1,7 @@
 package com.jerboa.ui.components.common
 
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,7 +15,12 @@ fun MenuItem(
     onClick: () -> Unit,
 ) {
     DropdownMenuItem(
-        text = { Text(text) },
+        text = {
+            Text(
+                text = text,
+                style = MaterialTheme.typography.bodyMedium,
+            )
+        },
         leadingIcon = {
             icon?.also { ico ->
                 InboxIconAndBadge(
