6bc42d30a047ba032afa65b70975605489c8c524
maarten.vercruysse <maarten.vercruysse@outlook.be>
2023-08-26 19:44:32 +0200

    Make nav text smaller

--- app/src/main/java/com/jerboa/ui/components/common/AppBars.kt
+++ app/src/main/java/com/jerboa/ui/components/common/AppBars.kt
@@ -39,6 +39,8 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import com.jerboa.R
 import com.jerboa.datatypes.samplePerson
@@ -131,6 +133,7 @@ fun BottomAppBarAll(
                 label = {
                     if (showTextDescriptionsInNavbar) {
                         Text(
+                            fontSize = TextUnit(10f, TextUnitType.Sp),
                             text = stringResource(tab.textId),
                             color = MaterialTheme.colorScheme.onSurface,
                         )
@@ -627,7 +630,10 @@ fun LoadingBar(
     padding: PaddingValues = PaddingValues(0.dp),
 ) {
     LinearProgressIndicator(
-        modifier = Modifier.fillMaxWidth().padding(padding).testTag("jerboa:loading"),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(padding)
+            .testTag("jerboa:loading"),
     )
 }
 
