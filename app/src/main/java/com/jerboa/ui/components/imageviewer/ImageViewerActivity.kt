304426cfe0b19e31df98724ac7d91b99072aca52
maarten.vercruysse <maarten.vercruysse@outlook.be>
2023-09-17 17:02:47 +0200

    Remove swipe to exit in ImageViewer

--- app/src/main/java/com/jerboa/ui/components/imageviewer/ImageViewerActivity.kt
+++ app/src/main/java/com/jerboa/ui/components/imageviewer/ImageViewerActivity.kt
@@ -8,9 +8,6 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.rememberScrollableState
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -75,9 +72,6 @@ fun ImageViewer(url: String, appState: JerboaAppState) {
     var showTopBar by remember { mutableStateOf(true) }
 
     val imageGifLoader = (ctx.applicationContext as JerboaApplication).imageViewerLoader
-    var debounce by remember {
-        mutableStateOf(false)
-    }
     val systemUiController = rememberSystemUiController()
 
     val window = (ctx as Activity).window
@@ -147,19 +141,7 @@ fun ImageViewer(url: String, appState: JerboaAppState) {
         content = {
             Box(
                 Modifier
-                    .background(backColor)
-                    .scrollable(
-                        orientation = Orientation.Vertical,
-                        state = rememberScrollableState(
-                            consumeScrollDelta = {
-                                if (it < -70 && !debounce) {
-                                    debounce = true
-                                    appState.navigateUp()
-                                }
-                                it
-                            },
-                        ),
-                    ),
+                    .background(backColor),
             ) {
                 if (imageState == ImageState.FAILED) {
                     Column(
