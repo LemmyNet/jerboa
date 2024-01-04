bbc330d07615467dc6011db2a3569751e09ff3f0
Maarten Vercruysse <67873169+MV-GH@users.noreply.github.com>
2024-01-04 17:54:07 +0100

    Fix initial default sort in community not being used (#1311)
    * Fix initial default sort in community not being used
    
    * Remove remnant
    
    * Fix lint

--- app/src/main/java/com/jerboa/model/HomeViewModel.kt
+++ app/src/main/java/com/jerboa/model/HomeViewModel.kt
@@ -1,42 +1,18 @@
 package com.jerboa.model
 
-import android.util.Log
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.asFlow
-import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.jerboa.api.ApiState
-import com.jerboa.db.entity.AnonAccount
 import com.jerboa.db.repository.AccountRepository
 import com.jerboa.jerboaApplication
-import com.jerboa.toEnumSafe
-import it.vercruysse.lemmyapi.v0x19.datatypes.PostResponse
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
 
-class HomeViewModel(private val accountRepository: AccountRepository) : PostsViewModel() {
-    private var likePostRes: ApiState<PostResponse> by mutableStateOf(ApiState.Empty)
-    private var savePostRes: ApiState<PostResponse> by mutableStateOf(ApiState.Empty)
-    private var deletePostRes: ApiState<PostResponse> by mutableStateOf(ApiState.Empty)
-
+class HomeViewModel(accountRepository: AccountRepository) : PostsViewModel(accountRepository) {
     val lazyListState = LazyListState()
 
     init {
-        viewModelScope.launch {
-            accountRepository.currentAccount
-                .asFlow()
-                .map { it ?: AnonAccount }
-                .collect { account ->
-                    updateSortType(account.defaultSortType.toEnumSafe())
-                    updateListingType(account.defaultListingType.toEnumSafe())
-                    Log.d("homeviewmodel", "Fetching posts")
-                    resetPosts()
-                }
-        }
+        init()
     }
 
     companion object {
