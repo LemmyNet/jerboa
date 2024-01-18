8d6568270ec2461a8af5363d4cc5ba8aff8cdf50
Dessalines <tyhou13@gmx.com>
2024-01-18 15:50:18 -0500

    Fixing jerboaappstate.

--- app/src/main/java/com/jerboa/MainActivity.kt
+++ app/src/main/java/com/jerboa/MainActivity.kt
@@ -61,8 +61,8 @@ import com.jerboa.ui.components.post.create.CreatePostActivity
 import com.jerboa.ui.components.post.edit.PostEditActivity
 import com.jerboa.ui.components.privatemessage.CreatePrivateMessageActivity
 import com.jerboa.ui.components.privatemessage.PrivateMessageReplyActivity
-import com.jerboa.ui.components.remove.post.PostRemoveActivity
 import com.jerboa.ui.components.remove.comment.CommentRemoveActivity
+import com.jerboa.ui.components.remove.post.PostRemoveActivity
 import com.jerboa.ui.components.report.comment.CreateCommentReportActivity
 import com.jerboa.ui.components.report.post.CreatePostReportActivity
 import com.jerboa.ui.components.settings.SettingsActivity
