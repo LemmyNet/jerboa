a9356649dbc405b814bfdf78a4a23088be8511a2
Dessalines <dessalines@users.noreply.github.com>
2024-02-10 15:44:46 -0500

    Remove material1 LocalContentColor. (#1366)

--- app/src/main/java/com/jerboa/ui/components/post/PostListing.kt
+++ app/src/main/java/com/jerboa/ui/components/post/PostListing.kt
@@ -18,8 +18,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.material.LocalContentAlpha
-import androidx.compose.material.LocalContentColor
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.Comment
 import androidx.compose.material.icons.filled.Bookmark
@@ -35,6 +33,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
@@ -47,7 +46,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
@@ -496,12 +494,9 @@ fun PostBody(
                             }
                         }
                     } else {
-                        val defaultColor: Color =
-                            LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
-
                         CreateMarkdownPreview(
                             markdown = text,
-                            defaultColor = defaultColor,
+                            color = LocalContentColor.current,
                             onClick = clickBody,
                             style = MaterialTheme.typography.bodyMedium,
                             modifier = Modifier.padding(MEDIUM_PADDING),
