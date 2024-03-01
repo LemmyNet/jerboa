ac39d872a805767d7910531c7619754744b670c8
Maarten Vercruysse <67873169+MV-GH@users.noreply.github.com>
2024-03-01 17:07:02 +0100

    Fix super and sub script not being properly rendered (#1410)
    * Better attempt
    
    * Fix it

---
+++ app/src/main/java/com/jerboa/util/markwon/ScriptRewriteSupportPlugin.kt
@@ -0,0 +1,26 @@
+package com.jerboa.util.markwon
+
+import io.noties.markwon.AbstractMarkwonPlugin
+
+class ScriptRewriteSupportPlugin : AbstractMarkwonPlugin() {
+    override fun processMarkdown(markdown: String): String {
+        return super.processMarkdown(
+            if (markdown.contains("^") || markdown.contains("~")) {
+                rewriteLemmyScriptToMarkwonScript(markdown)
+            } else { // Fast path: if there are no markdown characters, we don't need to do anything
+                markdown
+            },
+        )
+    }
+
+    companion object {
+        val SUPERSCRIPT_RGX = Regex("""\^([^\n^]+)\^""")
+        val SUBSCRIPT_RGX = Regex("""~([^\n~]+)~""")
+
+        fun rewriteLemmyScriptToMarkwonScript(text: String): String {
+            return text
+                .replace(SUPERSCRIPT_RGX, "<sup>$1</sup>")
+                .replace(SUBSCRIPT_RGX, "<sub>$1</sub>")
+        }
+    }
+}
