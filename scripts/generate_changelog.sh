b96eb92d2bde0e0393e1821b78677f5bebade307
Dessalines <dessalines@users.noreply.github.com>
2024-03-03 11:48:49 -0500

    Fix git cliff (#1419)

--- scripts/generate_changelog.sh
+++ scripts/generate_changelog.sh
@@ -9,9 +9,8 @@ new_tag="$1"
 last_tag=$(git describe --tags --abbrev=0)
 
 # Writing to the Releases.md asset that's loaded inside the app
-git cliff "$last_tag"..HEAD --tag "$new_tag" --output app/src/main/assets/RELEASES.md
-prettier -w app/src/main/assets/RELEASES.md
+git cliff --unreleased --tag "$new_tag" --output app/src/main/assets/RELEASES.md
 
 # Prepending to the RELEASES.md
-git cliff "$last_tag"..HEAD --tag "$new_tag" --prepend RELEASES.md
-prettier -w RELEASES.md
+git cliff --tag "$new_tag" --output RELEASES.md
+prettier -w RELEASES.md app/src/main/assets/RELEASES.md
