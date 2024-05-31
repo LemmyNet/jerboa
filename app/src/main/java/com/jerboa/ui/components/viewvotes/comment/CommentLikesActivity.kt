cbac0b5965f71ce2cd9ba0da5e15866de4b3088b
maarten.vercruysse <maarten.vercruysse@outlook.be>
2024-06-01 00:23:37 +0200

    Fix #1533 use M3 PullToRefresh

--- app/src/main/java/com/jerboa/ui/components/viewvotes/comment/CommentLikesActivity.kt
+++ app/src/main/java/com/jerboa/ui/components/viewvotes/comment/CommentLikesActivity.kt
@@ -1,23 +1,18 @@
 package com.jerboa.ui.components.viewvotes.comment
 
 import android.util.Log
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.pullrefresh.pullRefresh
-import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.zIndex
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.jerboa.JerboaAppState
 import com.jerboa.R
@@ -26,15 +21,13 @@ import com.jerboa.isScrolledToEnd
 import com.jerboa.model.CommentLikesViewModel
 import com.jerboa.ui.components.common.ApiEmptyText
 import com.jerboa.ui.components.common.ApiErrorText
-import com.jerboa.ui.components.common.JerboaPullRefreshIndicator
-import com.jerboa.ui.components.common.LoadingBar
+import com.jerboa.ui.components.common.JerboaLoadingBar
 import com.jerboa.ui.components.common.SimpleTopAppBar
-import com.jerboa.ui.components.common.isLoading
 import com.jerboa.ui.components.common.isRefreshing
 import com.jerboa.ui.components.viewvotes.ViewVotesBody
 import it.vercruysse.lemmyapi.v0x19.datatypes.CommentId
 
-@OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CommentLikesActivity(
     appState: JerboaAppState,
@@ -70,32 +63,15 @@ fun CommentLikesActivity(
                 }
             }
 
-            val refreshing = commentLikesViewModel.likesRes.isRefreshing()
-
-            val refreshState =
-                rememberPullRefreshState(
-                    refreshing = refreshing,
-                    onRefresh = {
-                        commentLikesViewModel.resetPage()
-                        commentLikesViewModel.getLikes(ApiState.Refreshing)
-                    },
-                )
-
-            Box(
-                modifier = Modifier.pullRefresh(refreshState)
-                    .padding(padding),
+            PullToRefreshBox(
+                modifier = Modifier.padding(padding),
+                isRefreshing = commentLikesViewModel.likesRes.isRefreshing(),
+                onRefresh = {
+                    commentLikesViewModel.resetPage()
+                    commentLikesViewModel.getLikes(ApiState.Refreshing)
+                },
             ) {
-                JerboaPullRefreshIndicator(
-                    refreshing,
-                    refreshState,
-                    Modifier
-                        .align(Alignment.TopCenter)
-                        .zIndex(100F),
-                )
-
-                if (commentLikesViewModel.likesRes.isLoading()) {
-                    LoadingBar()
-                }
+                JerboaLoadingBar(commentLikesViewModel.likesRes)
 
                 when (val likesRes = commentLikesViewModel.likesRes) {
                     ApiState.Empty -> ApiEmptyText()
