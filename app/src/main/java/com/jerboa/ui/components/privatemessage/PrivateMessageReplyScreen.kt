f3a62821ebb03798f071a24f8a71bea5b5ac6e53
maarten.vercruysse <maarten.vercruysse@outlook.be>
2025-05-11 23:43:29 +0200

    Also fix PrivateMessageReply

--- app/src/main/java/com/jerboa/ui/components/privatemessage/PrivateMessageReplyScreen.kt
+++ app/src/main/java/com/jerboa/ui/components/privatemessage/PrivateMessageReplyScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -52,11 +51,9 @@ fun PrivateMessageReplyScreen(
 
     var reply by rememberSaveable(stateSaver = TextFieldValue.Saver) { mutableStateOf(TextFieldValue("")) }
 
-    val focusManager = LocalFocusManager.current
-
     val loading =
         when (privateMessageReplyViewModel.createMessageRes) {
-            ApiState.Loading -> true
+            ApiState.Loading, is ApiState.Success -> true
             else -> false
         }
 
@@ -72,7 +69,6 @@ fun PrivateMessageReplyScreen(
                                 recipientId = privateMessageView.creator.id,
                                 content = reply.text,
                                 onGoBack = onBack,
-                                focusManager,
                             )
                         }
                     },
