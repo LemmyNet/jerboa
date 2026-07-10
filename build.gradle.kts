941bd29936cdf85f2e2dab458faea12a6739243d
maarten.vercruysse <maarten.vercruysse@outlook.be>
2026-07-10 21:40:11 +0200

    Force KGP 2.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,20 @@ subprojects {
     apply(plugin = "org.jmailen.kotlinter") // Version should be inherited from parent
 }
 
+// AGP 9.0 has a runtime dependency on the Kotlin Gradle plugin (KGP), defaulting to 2.2.10.
+// To use a higher KGP version, declare it here on the top-level buildscript classpath.
+// see https://developer.android.com/build/releases/agp-9-0-0-release-notes
+
+// If not forced to 2.4.0, ComposeMappingFile task fails since it will try to get 2.2.10 dependency
+// But that was only added in 2.3.0
+// https://kotlinlang.org/docs/whatsnew23.html#compose-compiler-stack-traces-for-minified-android-applications
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0")
+    }
+}
+
+
 // Enables compose compiler metrics
 // Generate them with `./gradlew assembleRelease --rerun-tasks -P enableComposeCompilerReports=true`
 // see https://github.com/androidx/androidx/blob/androidx-main/compose/compiler/design/compiler-metrics.md
